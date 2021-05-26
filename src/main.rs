faf453ac16f3f471f345590e0b4957b6f7134593
JPBeckner <pedrobeckner@hotmail.com>
2021-05-26 20:23:53 -0300

    wip: changing puck and main

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ pub use crate::puck::Puck;
 #[structopt(name = "Puck", about = "CLI to Punch the Clock")]
 enum PuckCli {
     // time in or time out
-    #[structopt(name = "punch", about = "Puch the clock and set the time that you ar comming (in) ou going (out)")]
+    #[structopt(name = "punch", about = "Puch the clock and set the time that you are comming (in) ou going (out)")]
     Punch {
         #[structopt(name = "io", about = "In or Out", required = true)]
         io: String,
@@ -34,49 +34,9 @@ enum PuckCli {
     },
 }
 
-// #[derive(StructOpt)]
-// struct Punch {
-//     #[structopt(name = "io", about = "In or Out", required = true)]
-//     io: String,
-//     #[structopt(name = "hour", about = "Hour to register", required = true)]
-//     hour: String,
-//     #[structopt(name = "user", about = "User to register in", required = false)]
-//     user: String,
-// }
-
-// #[derive(StructOpt)]
-// struct Balance {
-//         #[structopt(name = "year")]
-//         year: String,
-//         #[structopt(name = "month")]
-//         month: String,
-//         #[structopt(name = "day")]
-//         day: String,
-// }
-
-// #[derive(StructOpt)]
-// struct User {
-//     #[structopt(name = "new", about = "Register new user")]
-//     new: String,
-//     #[structopt(name = "default", about = "Set default user")]
-//     default: String,
-// }
-
-// struct PuckData {
-//     // usuario -> ano -> mes -> dia -> hora -> in/out
-//     user: String,
-//     year: String,
-//     month: String,
-//     day: String,
-//     hour: String,
-//     io: String,
-//     data: HashMap<String, HashMap<String, HashMap<String, HashMap<String, HashMap<String, String>>>>>,
-// }
-
 fn main() {
     match PuckCli::from_args() {
         PuckCli::Punch { io, hour, user } => {
-            // println!("io: {:?}, hour: {:?}", io, hour);
             let mut puck = Puck::new().expect("init failed.");
             puck.register(io, hour, user);
         },
--- src/puck.rs
+++ src/puck.rs
@@ -1,15 +1,20 @@
 use std::collections::HashMap;
 use chrono::{Datelike, Timelike, Utc, DateTime, NaiveTime};
 
+/*
+ideia: match get para cara nivel do json, e cada nivel popula uma variavel diferente.
+no final retorna Puck { user, ...}
+*/
+
 pub struct Puck {
-    data: Data
+    user: User
 }
 
 impl Puck {
     pub fn new() -> Result<Puck, std::io::Error> {
         println!("Create a new instance of puck");
         match Data::new() {
-            Ok(data) => Ok(Puck { data }),
+            Ok(data) => Ok(Puck { user:data.get_default_user() }),
             Err(e) => panic!("Error: {}", e)
         }
     }
@@ -20,27 +25,88 @@ impl Puck {
 
 }
 
-pub struct Data {
-    // user: String,
-    map: HashMap<String, String>,
+// usuario -> ano -> mes -> dia -> hora -> in/out
+struct Data {
+    default: String,
+    users: User,
+    // user: HashMap<String, String>,
+    // total_user_data: HashMap<String, user>,
+    // ano: HashMap<String, String>,
+    // mes: HashMap<String, String>,
+    // dia: HashMap<String, String>,
+    // hora: HashMap<String, String>,
+    // user: HashMap<String, HashMap<String, HashMap<String, HashMap<String, HashMap<String, String>>>>>
+}
+
+struct User {
+    user: HashMap<String, Ano>
+}
+
+struct Ano {
+    ano: HashMap<String, Mes>
+}
+
+struct Mes {
+    mes: HashMap<String, Dia>
+}
+
+struct Dia {
+    dia: HashMap<String, Hora>
+}
+
+struct Hora {
+    hora: HashMap<String, String>
 }
 
 impl Data {
-    pub fn new() -> Result<Data, std::io::Error> {
-                // open db.json
+    fn new() -> Result<Data, std::io::Error> {
+        // open db.json
         let file = std::fs::OpenOptions::new()
             .write(true)
             .create(true)
             .read(true)
             .open("db.json")?;
         // serialize json as HashMap
-        match serde_json::from_reader(file) {
-            Ok(map) => Ok(Data { map }),
+        match serde_json::from_reader::<_, HashMap<String, String>>(file) {
+            Ok(map) => {
+                let default: String;
+                match map.get("default") {
+                    Some(value) => default = String::from(value),
+                    None => default = String::from(""),
+                }
+                let users_avaliable: HashMap<String, Ano>;
+                match map.get("users") {
+                    Some(value) => users_avaliable = HashMap::new(),
+                    None => users_avaliable = HashMap::new()
+                }
+                let data = Data { default: default, users: User { user: users_avaliable } };
+                Ok(data)
+            },
             Err(e) if e.is_eof() => Ok(Data {
-                map: HashMap::new(),
+                default: String::new(),
+                users: User { user: HashMap::new() },
+                // <String, Ano { ano: HashMap<String, Mes { mes: HashMap<String, Dia { dia: HashMap<String, Hora { hora: HashMap<String, String>}>}> }>}>::new()
+                //String::new(), Ano { String::new(), Mes { String::new(), Dia { String::new(), Hora { String::new(), String::new() } } } }
+                // ano: HashMap::new(),
+                // mes: HashMap::new(),
+                // dia: HashMap::new(),
+                // hora: HashMap::new()
             }),
             Err(e) => panic!("An error occurred: {}", e),
 
         }
+    }
+
+    fn get_default_user(&mut self) -> User { // Result<User, std::io::Error>
+        match &self.users.user {
+            users_avaliable => {
+                User {user: users_avaliable}
+            },
+            //Err(e) => panic!("An error occured: {}", e),
+        }
+    }
+
+    fn add(&mut self, key: String, value: String) {
+        println!("Writing on file.");
     }
 }
